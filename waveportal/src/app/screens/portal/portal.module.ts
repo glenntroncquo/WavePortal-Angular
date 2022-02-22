c11d528a7a48c47f5aefde632825dd565c767ea3
Glenn Troncquo <32543129+glenntroncquo@users.noreply.github.com>
2022-02-22 19:48:25 +0100

    project setup

---
+++ waveportal/src/app/screens/portal/portal.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { PortalComponent } from './portal.component'
+
+@NgModule({
+  declarations: [PortalComponent],
+  imports: [CommonModule],
+})
+export class PortalModule {}
